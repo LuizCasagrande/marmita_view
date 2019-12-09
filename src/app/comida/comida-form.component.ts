ed5caa809a2d86e50c9e020d22aa05b079248e4a
LuizCasagrande <lzcsgrnd@gmail.com>
2019-12-09 11:34:21 -0300

    comida nao funciona

--- src/app/comida/comida-form.component.ts
+++ src/app/comida/comida-form.component.ts
@@ -3,12 +3,9 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ComidaService} from '../service/comida.service';
 import {MessageService} from 'primeng';
 import {Comida} from './comida';
-import {Ingrediente} from '../ingrediente/ingrediente';
-import {TipoComida} from './tipoComida';
 import {IngredienteService} from '../service/ingrediente.service';
-import {TipoService} from '../service/tipo.service';
 import {SelectItem} from 'primeng/api/selectitem';
-
+import {TipoService} from "../service/tipo.service";
 
 @Component({
     selector: 'app-comida-form',
@@ -18,51 +15,34 @@ import {SelectItem} from 'primeng/api/selectitem';
 export class ComidaFormComponent implements OnInit {
 
     objeto: Comida;
-    objeto2: TipoComida;
     ingredienteListSuggestions: SelectItem[];
-    selectedIngredientes1: Ingrediente[] = [];
-    tipoList: SelectItem[];
-    selectedTipos1: SelectItem[];
+    tipoListSuggestions: SelectItem[];
 
     constructor(private activatedRoute: ActivatedRoute,
                 private comidaService: ComidaService,
                 private router: Router,
                 private messageService: MessageService,
                 private ingredienteService: IngredienteService,
-                private tipoComidaService: TipoService) {
+                private tipoService: TipoService) {
         this.ingredienteService.findAll().subscribe(res => {
             this.ingredienteListSuggestions = res.map(ingrediente => {
                 return {label: ingrediente.ingredientes, value: ingrediente};
             });
         });
-        this.tipoComidaService.findAll().subscribe(res => {
-            this.tipoList = res.map(value => {
-                return {label: value.tipoComida, value: value};
-            });
-        });
+
+        this.tipoService.findAll().subscribe(res => {
+          this.tipoListSuggestions = res.map(tipo => {
+            return {label: tipo.tipoComida, value: tipo};
+          })
+        })
     }
 
     ngOnInit() {
         this.activatedRoute.queryParamMap.subscribe(params => {
             if (params.has('id')) {
-
                 this.comidaService.findOne(parseInt(params.get('id'))).subscribe(res => {
                     this.objeto = res;
-                    // this.selectedIngredientes1 = [];
-                    // for (const comidaIngrediente of this.objeto.ingredientesList) {
-                    //   this.selectedIngredientes1.push(comidaIngrediente.ingrediente);
-                    // }
-                    // console.log(this.selectedIngredientes1);
                 });
-
-                // this.tipoComidaService.findOne(parseInt(params.get('id'))).subscribe(res => {
-                //   this.objeto2 = res;
-                //   this.selectedTipos1 = [];
-                //   for (const comidaTipo of this.objeto.tipoList) {
-                //     this.selectedTipos1.push(comidaTipo.tipo);
-                //   }
-                // });
-
             } else {
                 this.resetaForm();
             }
@@ -70,13 +50,6 @@ export class ComidaFormComponent implements OnInit {
     }
 
     salvar(): void {
-        // this.objeto.ingredientesList = [];
-        // for (const ingrediente of this.selectedIngredientes1) {
-        //   const comidaIngrediente = new ComidaIngrediente();
-        //   comidaIngrediente.ingrediente = ingrediente;
-        //   this.objeto.ingredientesList.push(comidaIngrediente);
-        // }
-        // console.log(this.objeto);
         this.comidaService.save(this.objeto).subscribe(res => {
             this.objeto = res;
 
@@ -96,9 +69,8 @@ export class ComidaFormComponent implements OnInit {
 
     private resetaForm(): void {
         this.objeto = new Comida();
-        this.objeto.comida = '';
         this.objeto.ingredientesList = [];
+        this.objeto.tipoList = [];
     }
-
 }
 

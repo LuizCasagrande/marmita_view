5932d39cb096db2640d19bc0617084d7b69f73f6
usuario <juninhozatta221@gmail.com>
2019-12-13 15:12:59 -0300

    charles

--- src/app/relatorioPedidoCliente/relatorio-pedido-cliente.component.ts
+++ src/app/relatorioPedidoCliente/relatorio-pedido-cliente.component.ts
@@ -2,6 +2,7 @@ import {Component, Injector, OnInit} from '@angular/core';
 import {RelatorioPedidoClienteService} from "../service/relatorio-pedido-cliente.service";
 import {RelatorioPedidoCliente} from "./relatorioPedidoCliente";
 import {forkJoin} from "rxjs";
+import {MessageService} from "primeng";
 
 
 @Component({
@@ -19,24 +20,37 @@ export class RelatorioPedidoClienteComponent implements OnInit {
   filtrado: boolean = false;
 
 
-  constructor(private relatorioPedidoClienteService: RelatorioPedidoClienteService) {
+  constructor(private relatorioPedidoClienteService: RelatorioPedidoClienteService,
+              private messageService: MessageService) {
   }
 
   ngOnInit(): void {
   }
 
-  filtraData(){
-    forkJoin(
-      this.relatorioPedidoClienteService.getRelatorioCliente(this.dataInicial, this.dataFinal),
-      this.relatorioPedidoClienteService.relatorioPedidoClienteTotalAPagar(this.dataInicial, this.dataFinal),
-      this.relatorioPedidoClienteService.relatorioPedidoClienteTotalPago(this.dataInicial, this.dataFinal)
-    ).subscribe(res => {
-      this.relatorioPedidoClienteList = res[0];
-      this.totalAReceber = res[1];
-      this.totalRecebido = res[2];
-      this.filtrado = true;
-    });
+  filtraData() {
+    console.log("chegando aqui ")
+    if (this.dataInicial == null || this.dataFinal == null) {
+      this.messageService.add({
+        severity: 'info',
+        summary: "Complete as Informações Necessarias"
+      });
+    }else if(this.dataInicial == this.dataFinal){
+      this.messageService.add({
+        severity: 'info',
+        summary: "As datas informadas são iguais"
+      });
+    }else{
+      forkJoin(
+        this.relatorioPedidoClienteService.getRelatorioCliente(this.dataInicial, this.dataFinal),
+        this.relatorioPedidoClienteService.relatorioPedidoClienteTotalAPagar(this.dataInicial, this.dataFinal),
+        this.relatorioPedidoClienteService.relatorioPedidoClienteTotalPago(this.dataInicial, this.dataFinal)
+      ).subscribe(res => {
+        this.relatorioPedidoClienteList = res[0];
+        this.totalAReceber = res[1];
+        this.totalRecebido = res[2];
+        this.filtrado = true;
+      });
+    }
+
   }
-
-
 }

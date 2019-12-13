372a5416a9b3836cfb6bc628037ef7aaf7148b89
usuario <juninhozatta221@gmail.com>
2019-12-13 09:42:09 -0300

    relatorio cliente finaliz

--- src/app/service/relatorio-pedido-cliente.service.ts
+++ src/app/service/relatorio-pedido-cliente.service.ts
@@ -3,6 +3,7 @@ import {BaseService} from "./base.service";
 import {RelatorioPedidoCliente} from "../relatorioPedidoCliente/relatorioPedidoCliente";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +13,18 @@ export class RelatorioPedidoClienteService extends BaseService<RelatorioPedidoCl
     super(http, 'relatorio');
   }
 
-  getRelatorioCliente(dataInicial: Date, dataFinal: Date): Observable<[RelatorioPedidoCliente]>{
-    return this.http.get<[RelatorioPedidoCliente]>(`${this.getUrl()}/pedido-cliente?dataInicial=${dataInicial}&dataFinal=${dataFinal}`);
+  getRelatorioCliente(dataInicial: Date, dataFinal: Date): Observable<RelatorioPedidoCliente[]>{
+    return this.http.get<RelatorioPedidoCliente[]>(`${this.getUrl()}/pedido-cliente?dataInicial=${dataInicial}&dataFinal=${dataFinal}`);
+  }
+
+  relatorioPedidoClienteTotalAPagar(dataInicial: Date, dataFinal: Date): Observable<number>{
+    const url = `${this.getUrl()}/total-a-pagar?dataInicial=${dataInicial}&dataFinal=${dataFinal}`;
+    return this.http.get(url, { responseType: "text" }).pipe(map(res => parseInt(res)));
+  }
+
+  relatorioPedidoClienteTotalPago(dataInicial: Date, dataFinal: Date): Observable<number>{
+    const url = `${this.getUrl()}/total-pago?dataInicial=${dataInicial}&dataFinal=${dataFinal}`;
+    return this.http.get(url, { responseType: "text" }).pipe(map(res => parseInt(res)));
   }
 
 }
